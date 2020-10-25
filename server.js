0c621be328dfff652a92a1573987181b785396a6
Yaqeen hamd <engyaqeenhamd@yahoo.com>
2020-10-25 21:38:08 +0300

    try7

--- server.js
+++ server.js
@@ -6,38 +6,44 @@ function Location(city, locationData) {
     this.latitude = locationData.lat;
     this.longitude = locationData.lon;
 }
+function Weather(weatherData) {
+    // this.data = weatherData.city_name;
+    // this.city_name = weatherData.data.location.city;
+    // this.timezone=weatherData.timezone;
+    this.foreCast=description;
+    this.time=datetime;
+}
 // Defining Application Dependencies
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
-console.log(PORT);
 const app = express();
 app.use(cors());
 // Routes
 app.get('/', (reqeust, response) => {
-    response.send('Home Page Welcome to express');
+    response.status(200).send('Home Page Welcome to express');
 });
 app.get('/location', (request, response) => {
     const locationData = require('./data/location.json');
-    const city =request.query.city;
+    const city = request.query.city;
     let location;
     locationData.forEach(locationData => {
         location = new Location(city, locationData);
-        console.log(location);
     });
     response.json(location);
 });
-// app.get('/restaurant', (request, response)=>{
-//     const restaurantsData = require('./data/restaurants.json');
-//     // console.log(restaurantsData);
-//     let data = [];
-//     restaurantsData.nearby_restaurants.forEach(restaurant => {
-//         data.push(new Restaurant(restaurant));
-//     });
-//     response.json(data);
-// });
+app.get('/weather', (request, response)=>{
+    const weatherData = require('./data/weather.json');
+    let weather = [];
+    weatherData.data.forEach(element => {
+        let info=element.weather.description;
+        let time=element.datetime;
+        weather.push(new Weather(info,time));
+    });
+    response.json(weather);
+});
 app.use('*', (request, resp) => {
-    resp.status(404).send('Not found');
+  resp.status(404).send('Not found');
 });
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
